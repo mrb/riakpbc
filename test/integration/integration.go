0d4e407f3175bd0efad12ea102036053712b67c3
Brian Jones <mojobojo@gmail.com>
2013-08-06 06:27:28 +0900

    integration test tweaks

--- test/integration/integration.go
+++ test/integration/integration.go
@@ -11,10 +11,17 @@ type Data struct {
 	Data string `json:"data"`
 }
 
+type RiakData struct {
+	Email   string `json:"email" riak:"index"`
+	Twitter string `json:"twitter" riak:"index"`
+	Data    []byte `json:"data" riak:"index"`
+}
+
 func main() {
 	runtime.GOMAXPROCS(7)
 	cluster := []string{"127.0.0.1:8087", "127.0.0.1:8088", "127.0.0.1:8089", "127.0.0.1:8090"}
-	riak := riakpbc.NewClient(cluster)
+	coder := riakpbc.NewCoder("json", riakpbc.JsonMarshaller, riakpbc.JsonUnmarshaller)
+	riak := riakpbc.NewClientWithCoder(cluster, coder)
 
 	err := riak.Dial()
 	if err != nil {
@@ -26,6 +33,12 @@ func main() {
 
 	c := make(chan int)
 
+	riak_data := &RiakData{
+		Email:   "riak@example.com",
+		Twitter: "riak-twitter",
+		Data:    []byte("riak-data"),
+	}
+
 	for g := 0; g < 7; g++ {
 		go func(which int) {
 			log.Print("<", which, "> Loaded")
@@ -35,21 +48,25 @@ func main() {
 				times = times + 1
 				_, err := riak.StoreObject("bucket", "data", "{'ok':'ok'}")
 				if err != nil {
+					log.Print(err.Error())
 					errs = errs + 1
 				}
 
 				_, err = riak.SetClientId("coolio")
 				if err != nil {
+					log.Print(err.Error())
 					errs = errs + 1
 				}
 
 				_, err = riak.GetClientId()
 				if err != nil {
+					log.Print(err.Error())
 					errs = errs + 1
 				}
 
 				data, err := riak.FetchObject("bucket", "data")
 				if err != nil {
+					log.Print(err.Error())
 					errs = errs + 1
 				} else {
 					if string(data.GetContent()[0].GetValue()) != "{'ok':'ok'}" {
@@ -59,11 +76,25 @@ func main() {
 
 				_, err = riak.StoreObject("bucket", "moreData", "stringData")
 				if err != nil {
+					log.Print(err.Error())
 					errs = errs + 1
 				}
 
 				_, err = riak.FetchObject("bucket", "moreData")
 				if err != nil {
+					log.Print(err.Error())
+					errs = errs + 1
+				}
+
+				_, err = riak.StoreStruct("riakpbctestbucket", "testkey_struct", riak_data)
+				if err != nil {
+					log.Print(err.Error())
+					errs = errs + 1
+				}
+
+				_, err = riak.DeleteObject("riakpbctestbucket", "testkey_struct")
+				if err != nil {
+					log.Print(err.Error())
 					errs = errs + 1
 				}
 
